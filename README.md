e1beb0b90216bc281fe1ec3b42695cba9910089a
André Luiz Albano <andre_barros@id.uff.br>
2024-01-24 13:43:48 -0300

    Added 3 inversions

--- README.md
+++ README.md
@@ -2,31 +2,125 @@
 Repository for comparing different inversion methods at a section at the Parque das Baleias
 
 # 1 - Método dos Mínimos Quadrados
-<center>
 
 O primeiro método utilizado para inversão é o Método dos Mínimos Quadrados, das equações normais, que é implementado pela seguinte função:
+<center>
 
 `pylops.optimization.leastsquares.normal_equations_inversion`
+
+</center>
 
 Que é uma implementação das equações normais:
 
 $$ m = (G^{T}G)^{-1}G^{T}y $$
 
-
-Esse método implementa os sistemas de equação de forma explícita, e resolve eles usando um esquema iterativo útil para matrizes quadradas. 
-
+Esse método implementa os sistemas de equação de forma explícita, e resolve eles usando um esquema iterativo útil para matrizes quadradas.
+<center>
 # Situação inicial
 
 <img src="Figures/lowfrequency_497.png" width=800 height=600>
 
 # Resultado final
+
 Seção
-<img src="Figures/image_inversion_497_well.png" alt="Alt text" width=800 height=600>
+
+<img src="Figures/image_inversion_497_well_ls.png" alt="Alt text" width=800 height=600>
 
 Plot de comparação
 
-<img src="Figures/inversion_497_well.png" alt="Alt text" width=800 height=300>
+<img src="Figures/comparison_plot_ls.png" alt="Alt text" width=800 height=300>
 
-O código rodou em 9.63s
+O código rodou em 8.28s
+</center>
 
-# 2 - Método dos Mínimos Quadrados
+# 2 - Mínimos Quadrados com Regularização
+
+O método de regularização empregado usa o operador da segunda derivada, conforme expresso pela fórmula:
+
+$$ x = (\textbf{R}^{T}\textbf{R} + \epsilon^{2}_{\nabla} \nabla^{T}\nabla)^{-1}\textbf{R}^{T}y $$
+
+Essa abordagem é implementada por meio da classe `pylops.SecondDerivative`
+
+A função utilizada, `pylops.optimization.leastsquares.regularized_inversion`, resolve o seguinte problema:
+
+$$ \begin{split}\begin{bmatrix}
+    \mathbf{R}    \\
+    \epsilon_\nabla \nabla
+\end{bmatrix} \mathbf{x} =
+    \begin{bmatrix}
+    \mathbf{y}    \\
+    0
+\end{bmatrix}\end{split} $$
+
+```python
+D2op = pylops.SecondDerivative([nz, nx], axis=1, dtype='float64')
+
+# Regularized function
+m_inv_reg, _, _, _, _ = pylops.optimization.leastsquares.regularized_inversion(
+    Op=PPop, # operador de modelagem direta (G)
+    y = amp_ls.ravel()/30000, # dado observado.
+    Regs=[D2op], # regularization operator
+    x0 = mback_ls.ravel(), # modelo inicial
+    **dict(damp=9e-1, iter_lim=1000, show=0))
+
+m_inv_reg = np.exp(m_inv_reg.reshape(nz, nx))
+```
+Nota: O mesmo resultado pode ser alcançado utilizando a `pylops.optimization.leastsquares.normal_equations_inversion`
+
+Os parâmetros utilizados para o damping, que seria o Tikhonov damping é de 0.9, o que faz com que o modelo seja o mais próximo do módelo observado.
+
+O resultado é o seguinte:
+
+<center>
+<img src="Figures/image_inversion_497_well_regularized.png" alt="Alt text" width=800 height=600>
+
+<img src="Figures/comparison_plot_ls_reg.png" alt="Alt text" width=800 height=300>
+</center>
+
+Como se pode observar, o resultado está mais suavizado que o sem a regularização. Isso se dá pelo valor do damping, que é mais alto que o utilizado no método anterior. Quanto mais alto esse valor, mais suavizado o resultado.
+
+O código rodou em 8.86s
+
+# 2 - Método FISTA
+
+Esse método utiliza da Transformada de Fourier para resolver o problema de inversão. A função utilizada é a `pylops.optimization.leastsquares.fista`, e o erro utilizado é de 0.028.
+
+```python
+m_inv_fista = pylops.optimization.sparsity.fista(
+    PPop,
+    y = amp_ls.ravel()/30000,
+    x0 = mback_ls.ravel(),
+    niter=1000,
+    eps=0.028,
+    tol=1e-5
+    )[0]
+
+m_inv_fista = np.exp(mback_ls + m_inv_fista.reshape(nz, nx))
+```
+O resultado é o seguinte:
+
+<center>
+<img src="Figures/image_inversion_497_well_fista.png" alt="Alt text" width=800 height=600>
+</center>
+
+# 4 - Método com pré-condicionamento
+
+O método de pré-condicionamento é utilizado para melhorar a convergência do método de Mínimos Quadrados. A função utilizada é a `pylops.optimization.leastsquares.preconditioned_inversion`, e o erro utilizado foi de 1e-1.
+
+```python
+m_inv_prec = pylops.optimization.leastsquares.preconditioned_inversion(
+    PPop,
+    y = amp_ls.ravel()/30000,
+    P = Sop,
+    **dict(damp=np.sqrt(1e-1), iter_lim=1000, show=0)
+)[0]
+```
+<center>
+<img src="Figures/image_inversion_497_well_prec.png" alt="Alt text" width=800 height=600>
+</center>
+
+Comparação entre as inversões:
+
+<center>
+<img src="Figures/all_comparison_plots.png" alt="Alt text" width=800 height=300>
+</center>
--- least_squares_method.py
+++ least_squares_method.py
@@ -1,6 +1,6 @@
 import sys
-import time
 import os
+import time
 import numpy as np
 import pandas as pd
 import matplotlib
@@ -10,43 +10,40 @@ import scipy.signal
 import scipy.interpolate
 import pylops
 import seaborn as sns
+import scipy.linalg
+from numpy.linalg import inv
 import warnings
 warnings.filterwarnings("ignore")
 
-sns.set_theme() # setando o tema dos plots
+sns.set_theme()
+
 print("Starting...")
 
 # iniciando timer para cronometrar o script.
 start_time = time.time()
 
-# leitura dos arquivos
-eoceno = pd.read_csv("Superficies/EocenoSuperior_SubvolBackground_Time.dat",
-                     delim_whitespace=True,
-                     skiprows=0,
-                     usecols=(0, 1, 2),
-                     names=['X', 'Y', 'Z'])
-
-paleoceno = pd.read_csv("Superficies/Paleoceno_SubvolBackground_Time.dat",
-                        delim_whitespace=True,
-                        skiprows=0,
-                        usecols=(0, 1, 2),
-                        names=['X', 'Y', 'Z'])
-
 print("Loading data...")
-
-# determinação das variáveis
-# amplitude
-amp = np.load("Sismica/sismica_497.npy")
-# impedancia de baixa frequencia 
-mback = np.load("Impedancia/lowfrequency_497.npy")
-# wavelet 
-wav = np.load("Wavelets/wav_avg.npy")
-
+# importação das superfícies
+eoceno = pd.read_csv("Superficies/EocenoSuperior_SubvolBackground_Time.dat",
+                     delim_whitespace=True, skiprows=0, usecols=(0, 1, 2), names=['X', 'Y', 'Z'])
+print("Eoceno loaded")
+paleoceno = pd.read_csv("Superficies/Paleoceno_SubvolBackground_Time.dat",
+                        delim_whitespace=True, skiprows=0, usecols=(0, 1, 2), names=['X', 'Y', 'Z'])
+print("Paleoceno loaded")
+# determinação dos limites
 xl_start = np.min(eoceno['Y'])
 xl_end = np.max(eoceno['Y'])
 
-# Seismic section of well 55
+print("loading npy...")
+amp = np.load("Sismica/sismica_497.npy")
+mback = np.load("Impedancia/lowfrequency_497.npy") #
+wav = np.load("Wavelets/wav_avg.npy")
+print(f" amp shape {np.shape(amp)}")
+print(f" mback shape {np.shape(mback)}")
+print(f" wav shape {np.shape(wav)}")
+
 print("Plotting low frequency...")
+
 fig, axs = plt.subplots(2, 1, figsize=(10, 8))
 
 c=axs[0].imshow(amp.T, aspect='auto', cmap='gray_r', vmin=-15000, vmax=15000, extent=[xl_start, xl_end, 7000, 0])
@@ -76,7 +73,9 @@ axs[0].set_xticks([])
 plt.tight_layout()
 plt.savefig('Figures/lowfrequency_497.png', dpi=200, bbox_inches='tight')
 
-print("Creating wavelet...")
+print("Loading wavelet config...")
+
+# Convertendo de tempo para profundidade
 
 path = "Pocos/IP_UPS/"
 
@@ -85,71 +84,53 @@ names = ['Depth', 'VP', 'RHO', 'VP_4Hz', 'RHO_4Hz', 'VP_6Hz', 'RHO_6Hz',
          'VP_8Hz', 'RHO_8Hz', 'IP', 'IP_4Hz', 'IP_6Hz', 'IP_8Hz']
 
 for file in os.listdir(path):
-    well = pd.read_csv(path+file, delim_whitespace=True, skiprows=1, names=names)
-    # Replacing nans
+    well = pd.read_csv(path+file,
+                       delim_whitespace=True,
+                       skiprows=1,
+                       names=names)
+
+    # Replacing nans. The value 1e30 is a null value.
     well.replace(1e30, np.nan, inplace=True)
+
     # Naming well
     well['WELL'] = file.split('_')[0]
-    tdr = pd.read_csv(f'Pocos/TimeVsDepth/{file.split("_")[0]}.dat', skiprows=14, delim_whitespace=True,
+    time_vs_depth = pd.read_csv(f'Pocos/TimeVsDepth/{file.split("_")[0]}.dat', skiprows=14, delim_whitespace=True,
                       names=['MD', 'TVD', 'TVDSS', 'TWT', 'VINT'])
-    interp_tdr = scipy.interpolate.interp1d(tdr['MD'], tdr['TWT'], fill_value='extrapolate', bounds_error=False)
-    t_well = interp_tdr(well.Depth)
+    interp_time_vs_depth = scipy.interpolate.interp1d(time_vs_depth['MD'], time_vs_depth['TWT'],
+                                                      fill_value='extrapolate', bounds_error=False)
+    t_well = interp_time_vs_depth(well.Depth)
 
     new_samples = np.arange(2700, 3900, 4)
     interp2 = scipy.interpolate.interp1d(t_well, well.IP, bounds_error=False, fill_value='extrapolate')
-    ip_ups = interp2(new_samples)
+    ip_ups = interp2(new_samples) # impedance upsampled
     ip_ups = scipy.ndimage.gaussian_filter(ip_ups, sigma=0.5)
 
     arr = np.array([new_samples, ip_ups])
     nwell = pd.DataFrame(arr.T, columns=['TWT', 'IP_UPS'])
     nwell['WELL'] = file.split('_')[0]
     nwell['IP_UPS'] = nwell['IP_UPS'].fillna(method='bfill').fillna(method='ffill')
-    plt.title(nwell['WELL'].unique())
-    #plt.plot(nwell['IP_UPS'])
-    #plt.show()
-    # Concatenate
+    # for i in nwell.WELL.unique():
+    #     plt.title(i)
+    #     plt.plot(nwell[nwell['WELL'] == i]['IP_UPS'])
+    #     plt.savefig(f'Figures/{i}.png', dpi=200, bbox_inches='tight')
+    #     plt.clf()
     df = pd.concat([df, nwell], ignore_index=True)
 
-############################### Inversão usando norma L1 #################################################
+print("Inverting data using least squares method without regularization...")
 
-nd = len(mback) # impedancia
-ns = len(wav) # wavelet
-
-S = scipy.linalg.toeplitz(np.append(wav, np.zeros(2*nd-ns)), np.zeros(nd))[(ns)//2:nd+(ns)//2]
-
-matder = np.zeros(shape=(nd,nd))
-
-for i in range(nd):
-  matder[i][i] = -1
-  if i<nd-1:
-    matder[i][i+1] = 1
-  else:
-    matder[i][i] = 0
-                
-print(matder)
-print(np.shape(matder))
-
-
-refl = 0.5 * matder @ np.log(mback)
-traco = S @ refl
-
-plt.plot(amp,traco)
-
-# def inv_normaL1(dobs, G, miL1=0.00001):
-    
-
-
-
-
-print("Inverting data using least squares method...")
 amp_ls = amp.copy()
 mback_ls = mback.copy()
+
+# Inverting at well 55 inline
+'''
+O código está pegando todas as linhas e as colunas de 675 a 974 da matriz `mback_ls`.
+'''
 
 amp_ls = amp[:, 675:975].T
 mback_ls = np.log(mback[:, 675:975].T)
 
-print("Shape of data [n_samples, n_traces]:", amp_ls.shape)
-
+print("Shape of amp_ls [n_samples, n_traces]:", amp_ls.shape)
+print("Shape of mback_ls [n_samples, n_traces]:", mback_ls.shape)
 
 # Wavelet operator
 nz, nx = amp_ls.shape
@@ -158,28 +139,37 @@ PPop = pylops.avo.poststack.PoststackLinearModelling(wav/2,
                                                      spatdims=nx,
                                                      explicit=True)
 
-# Trace-by-trace
-minv_tbt, _ = pylops.optimization.leastsquares.normal_equations_inversion(
-    Op=PPop,
-    y=amp_ls.ravel() / 30000,
-    Regs=None,
+# Trace-by-trace - y = Op * m
+m_inv_tbt, _ = pylops.optimization.leastsquares.normal_equations_inversion(
+    Op=PPop, # operador de modelagem direta (G)
+    y = amp_ls.ravel() / 30000, # dado observado.
+    Regs=[],
     epsI=5e-1,
-    x0=mback_ls.ravel(),
+    x0 = mback_ls.ravel(), # modelo inicial
     engine='scipy',
-    **dict(maxiter=1000))
-minv_tbt = np.exp(mback_ls + minv_tbt.reshape(nz, nx))
+    **dict(maxiter=1000, show=True))
+m_inv_tbt = np.exp(mback_ls + m_inv_tbt.reshape(nz, nx))
+
+print("Inversion done, saving results...")
 
 img = np.tile(df.loc[df.WELL=='6-BRSA-497-ESS'].IP_UPS, (100, 1))
 
+print("Saving plots...")
 # Seismic section of well 55
-print("Plotting final inversion...")
+
 fig, axs = plt.subplots(2, 1, figsize=(10, 8))
 
-c=axs[0].imshow(amp_ls, aspect='auto', cmap='gray_r', vmin=-15000, vmax=15000, extent=[xl_start, xl_end, 3900, 2700])
+# amplitude
+c=axs[0].imshow(amp_ls, aspect='auto', cmap='gray_r', vmin=-15000,
+                vmax=15000, extent=[xl_start, xl_end, 3900, 2700])
 plt.colorbar(c, ax=axs[0], pad=0.01).set_label('Amplitude', size=15)
 
-c=axs[1].imshow(minv_tbt, aspect='auto', cmap='viridis_r', vmin=5000, vmax=8000, extent=[xl_start, xl_end, 3900, 2700])
+# inversion trace-by-trace
+c=axs[1].imshow(m_inv_tbt, aspect='auto', cmap='viridis_r', vmin=5000,
+                vmax=8000, extent=[xl_start, xl_end, 3900, 2700])
 plt.colorbar(c, ax=axs[1], pad=0.01).set_label('P-Impedance [m/s.g/cm³]', size=15)
+
+# well impedance
 axs[1].imshow(img.T, aspect='auto', cmap='viridis_r', vmin=5000, vmax=8000,
              extent=(5553-5, 5553+5, 3900, 2700), zorder=20)
 
@@ -201,13 +191,14 @@ for ax in [axs[0], axs[1]]:
 axs[0].legend(ncol=3, loc='lower center', bbox_to_anchor=(0.75, 1.0), facecolor='white', edgecolor='white')
 axs[0].set_xticks([])
 plt.tight_layout()
-plt.savefig('Figures/image_inversion_497_well.png', dpi=200, bbox_inches='tight')
+plt.savefig('Figures/image_inversion_497_well_ls.png', dpi=200, bbox_inches='tight')
 
 plt.figure(figsize=(8, 3))
 
 plt.plot(df.loc[df.WELL=='6-BRSA-497-ESS'].TWT, df.loc[df.WELL=='6-BRSA-497-ESS'].IP_UPS, label='Obs', lw=2.0)
 plt.plot(df.loc[df.WELL=='6-BRSA-497-ESS'].TWT, np.exp(mback_ls[:, 5553-xl_start]), label='Back', lw=1.6, ls='-.')
-plt.plot(df.loc[df.WELL=='6-BRSA-497-ESS'].TWT, minv_tbt[:, 5553-xl_start], label='Tbt', lw=1.6, ls='--')
+plt.plot(df.loc[df.WELL=='6-BRSA-497-ESS'].TWT, m_inv_tbt[:, 5553-xl_start], label='Tbt', lw=1.6, ls='--')
+# plt.plot(df.loc[df.WELL=='6-BRSA-497-ESS'].TWT, m_inv_reg[:, 5553-xl_start], label='Tbt', lw=1.6, ls='--')
 #plt.plot(new_twt, minv_hop[:, 1469-new_xl_start], label='Hor', lw=1.6, ls='--')
 #plt.plot(new_twt, minv_dop[:, 1469-new_xl_start], label='Dir', lw=1.6, ls='--')
 #plt.plot(new_twt, bsd[1269 - new_il_start, 1469-new_xl_start, :], label='Petrel', lw=1.6, ls='--')
@@ -216,7 +207,7 @@ plt.legend(loc='lower right', bbox_to_anchor=(1.0, 1.0), edgecolor='w', facecolo
 
 plt.xlim(3000, 3600)
 plt.ylim(4000, 10000)
-plt.savefig('Figures/inversion_497_well.png', dpi=200, bbox_inches='tight')
+plt.savefig('Figures/comparison_plot_ls.png', dpi=200, bbox_inches='tight')
 
 end_time = time.time()
 print("Done!")
